9ea4d543c5fa209c7235394dc25042ab02b55384
SBereno <percival1999@gmail.com>
2019-11-14 19:31:15 +0100

    ADD Funcionalidad Server

--- src/pp/FramePrincipal.java
+++ src/pp/FramePrincipal.java
@@ -5,21 +5,7 @@ import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -128,36 +114,7 @@ public class FramePrincipal extends JFrame {
 			introducirComanda(intFrame, numeroTaules);
 			Component tab = intFrame;
 			tabbedPane.addTab("Taula" + i, tab);
-		}
-
-		/*
-		 * SERVER try { System.out.println("LocalHost = " +
-		 * InetAddress.getLocalHost().toString()); } catch (UnknownHostException uhe) {
-		 * System.err.println("No puedo saber la dirección IP local : " + uhe); } //
-		 * Abrimos un "Socket de Servidor" TCP en el puerto 4444. ServerSocket
-		 * serverSocket = null;
-		 * 
-		 * try { serverSocket = new ServerSocket(4444); } catch (IOException ex) {
-		 * System.out.println("Can't setup server on this port number. "); }
-		 * 
-		 * Socket socket = null; InputStream in = null; OutputStream out = null;
-		 * 
-		 * try { socket = serverSocket.accept(); } catch (IOException ex) {
-		 * System.out.println("Can't accept client connection. "); }
-		 * 
-		 * try { in = socket.getInputStream(); } catch (IOException ex) {
-		 * System.out.println("Can't get socket input stream. "); }
-		 * 
-		 * try { out = new FileOutputStream("Comandes" + File.separatorChar +
-		 * "aaa.txt"); } catch (FileNotFoundException ex) {
-		 * System.out.println("File not found. "); }
-		 * 
-		 * byte[] bytes = new byte[16 * 1024];
-		 * 
-		 * int count; try { while ((count = in.read(bytes)) > 0) { out.write(bytes, 0,
-		 * count); } out.close(); in.close(); socket.close(); serverSocket.close(); }
-		 * catch (IOException e) { e.printStackTrace(); }
-		 */
+		} 
 	}
 
 	public static void refreshFrame() {
---
+++ src/pp/Server.java
@@ -0,0 +1,75 @@
+package pp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Server {
+
+	public static void main(String[] args) {
+		Server sv = new Server();
+	}
+
+	public Server() {
+		try {
+			System.out.println("LocalHost = " + InetAddress.getLocalHost().toString());
+		} catch (UnknownHostException uhe) {
+			System.err.println("No puedo saber la dirección IP local : " + uhe);
+		}
+		// Abrimos un "Socket de Servidor" TCP en el puerto 4444.
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(4444);
+		} catch (IOException ex) {
+			System.out.println("Can't setup server on this port number. ");
+		}
+
+		Socket socket = null;
+		InputStream in = null;
+		OutputStream out = null;
+		boolean repetir = true;
+		while (repetir) {
+
+			try {
+				socket = serverSocket.accept();
+			} catch (IOException ex) {
+				System.out.println("Can't accept client connection. ");
+			}
+
+			try {
+				in = socket.getInputStream();
+			} catch (IOException ex) {
+				System.out.println("Can't get socket input stream. ");
+			}
+
+			try {
+				out = new FileOutputStream("Comandes" + File.separatorChar + "ComandaTaula2.xml");
+			} catch (FileNotFoundException ex) {
+				System.out.println("File not found. ");
+			}
+
+			byte[] bytes = new byte[16 * 1024];
+
+			int count;
+			try {
+				while ((count = in.read(bytes)) > 0) {
+					out.write(bytes, 0, count);
+				}
+				System.out.println("Comanda recibida");
+				out.close();
+				in.close();
+				socket.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
